d5b7662331f49ca1edc74a68f3c59fa12180a714
klesio <klesio.dev@gmail.com>
2024-01-31 01:47:14 -0300

    feat(extension): Component styling adjustments and update code

---
+++ src/app/components/Search.tsx
@@ -0,0 +1,86 @@
+import Head from 'next/head';
+
+const Search = () => {
+  return (
+    <div style={{ width : '100%' }}>
+      <Head>
+        <title>Shopee</title>
+      </Head>
+
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          border: '1px solid #a0aec0',
+          padding: '1.25rem',
+          borderRadius: '0.375rem',
+          boxShadow:
+            '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+         
+          maxWidth: '280px',
+          margin: '0 auto',
+          gap: '2rem',
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginBottom: '1rem',
+            position: 'relative',
+          }}
+        >
+          <div>
+            <img
+              src="https://www.pngmart.com/files/12/Shopee-Logo-Transparent-PNG.png"
+              alt="Shopee Logo"
+              style={{ width: '5rem', height: '5rem' }}
+            />
+          </div>
+          <div
+            style={{
+              fontSize: '1.5rem',
+              fontWeight: 'bold',
+              color: '#4a5568',
+              cursor: 'pointer',
+              position: 'absolute',
+              top: '-0.5rem',
+              right: '0',
+            }}
+          >
+            X
+          </div>
+        </div>
+
+        <div>
+          <input
+            style={{
+              maxWidth: '100%',
+              padding: '0.5rem',
+              border: '1px solid #e2e8f0',
+              borderRadius: '0.25rem',
+              marginBottom: '1rem',
+            }}
+            type="text"
+            placeholder="Busque aqui"
+          />
+          <button
+            style={{
+              width: '100%',
+              padding: '0.75rem',
+              backgroundColor: '#4299e1',
+              color: '#ffffff',
+              borderRadius: '0.25rem',
+              fontWeight: 'bold',
+            }}
+          >
+            Grab
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,18 @@
-import Head from 'next/head';
+import Search from './components/Search';
 
 export default function Home() {
   return (
-    <main className="flex justify-center min-h-screen bg-gray-100 p-5">
-      <Head>
-        <title>Shopee</title>
-      </Head>
-
-      <div className="flex flex-col border-sky-100 p-5 rounded-lg shadow-md w-4/5 md:w-3/5 bg-white">
-        <div className="flex items-center justify-between mb-4">
-          <div>
-            <img
-              src="https://brandlogovector.com/wp-content/uploads/2021/05/Shopee-Logo-Small.png"
-              alt="Shopee Logo"
-              className="w-44 h-44"
-            />
-          </div>
-          <div className="text-xl font-bold text-gray-500 cursor-pointer">
-            X
-          </div>
-        </div>
-
-        <div>
-          <input
-            className="w-full p-2 border border-gray-200 rounded-md mb-4"
-            type="text"
-            placeholder="Busque aqui"
-          />
-          <button className="w-full py-2 bg-blue-500 text-white rounded-md font-bold">
-            Grab
-          </button>
-        </div>
-      </div>
+    <main
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        minHeight: '70vh',
+        maxWidth: "100%",
+        backgroundColor: 'rgb(245, 245, 245)',
+        padding: '2rem',
+      }}
+    >
+      <Search />
     </main>
   );
 }
